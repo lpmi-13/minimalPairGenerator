4bcffeef5aae29171d5ae9e3813170b70751d131
lpmi-13 <leskis@gmail.com>
2018-01-15 22:20:20 +0000

    add initial step converting other audio format (webm) to mp3

---
+++ convert_to_mp3.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+ORIGINAL_AUDIO_DIR = './original_audio'
+CONVERTED_AUDIO_DIR = './converted_audio'
+
+audio_dir = os.listdir(ORIGINAL_AUDIO_DIR)
+
+if not os.path.exists(CONVERTED_AUDIO_DIR):
+    os.mkdir(CONVERTED_AUDIO_DIR)
+
+for audio_file in audio_dir:
+    filename, file_extension = os.path.splitext(audio_file)
+
+    '''
+    not actually sure if this will convert anything to mp3,
+    since only tested with webm, but theoretically could work
+    '''
+    command = ['ffmpeg', '-i', audio_file, '-acodec', 'libmp3lame', '-aq', '2', CONVERTED_AUDIO_DIR + '/' filename + '.mp3']
