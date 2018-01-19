835b0d6fec16dc09f180e18f203c816df439bbce
lpmi-13 <leskis@gmail.com>
2018-01-19 22:34:23 +0000

    add additional metadata regarding vowel vs consonant

--- generate_assets.py
+++ generate_assets.py
@@ -3,20 +3,31 @@ import os
 import subprocess
 
 DATA_DIR = './data/'
+VOWEL_PHONEMES = {'æ', 'ɪ', 'ɑ', 'ʌ', 'ʊ', 'ɛ', 'i', 'ɔ', 'u', 'o','a', 'ə', 'e'}
 
 directory_json = {}
 
 for subdir in os.listdir(DATA_DIR):
     directory_path = DATA_DIR + subdir
 
-    keys = directory_json.setdefault(subdir, [])
+    # splits phonemes into vowels and consonants
+    firstPhoneme, secondPhoneme = subdir.split('-')
+    if (firstPhoneme in VOWEL_PHONEMES and secondPhoneme in VOWEL_PHONEMES):
+
+        vowel = True
+
+    else:
+
+        vowel = False
+
+    json_item = {'vowel' : vowel, 'words' : []}
 
     pair_directories = os.listdir(directory_path)
 
     for sub_subdir in pair_directories:
 
         # build up the json describing the directory structure
-        keys.append(sub_subdir)
+        json_item['words'].append(sub_subdir)
 
         # generate the compiled audio assets
         sounds_dir = DATA_DIR + subdir + '/' + sub_subdir + '/'
@@ -25,6 +36,8 @@ for subdir in os.listdir(DATA_DIR):
         command = ['waudsprite', '--loop', 'loop', '--autoplay', 'loop', '-o', m4a_file_name, '-e', 'm4a', sounds_dir + '/*.mp3']
 
         subprocess.call(command)
+
+    directory_json[subdir] = json_item
 
 with open('directory.json','w') as f:
     json.dump(directory_json, f)
