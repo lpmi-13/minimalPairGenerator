a22ab152c8f467c3433bba613ebd94a96315583b
lpmi-13 <leskis@gmail.com>
2018-01-21 21:59:39 +0000

    add output during run to update on progress

--- force-align.py
+++ force-align.py
@@ -8,6 +8,9 @@ CONVERTED_AUDIO_DIR = 'converted_audio/'
 CONVERTED_TRANSCRIPT_DIR = 'converted_transcripts/'
 FORCE_ALIGNMENT_JSON_DIR = 'force_aligned_json/'
 
+number_of_files = len(os.listdir(CONVERTED_AUDIO_DIR))
+progress_count = 0
+
 if not os.path.exists(FORCE_ALIGNMENT_JSON_DIR):
     os.mkdir(FORCE_ALIGNMENT_JSON_DIR)
 
@@ -17,3 +20,7 @@ for audio_file in os.listdir(CONVERTED_AUDIO_DIR):
     curl_command = ['curl', '-F', 'audio=@' + os.path.join(CONVERTED_AUDIO_DIR, audio_file), '-F', 'transcript=@' + os.path.join(CONVERTED_TRANSCRIPT_DIR, filename + '.txt'), 'http://127.0.0.1:' + DOCKER_PORT + '/transcriptions?async=false', '-o', os.path.join(FORCE_ALIGNMENT_JSON_DIR, filename + '.json')]
 
     subprocess.call(curl_command)
+
+    progress_count += 1
+    
+    print('current progress is {}/{} files'.format(progress_count, number_of_files)
